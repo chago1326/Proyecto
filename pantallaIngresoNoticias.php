78ac3aa1bb7e7de43585df66e164c205e5b358ed
chago1326 <ricardomillon6@gmail.com>
2022-03-21 02:34:37 -0600

    Realice algunos cambios

--- pantallaIngresoNoticias.php
+++ pantallaIngresoNoticias.php
@@ -1,6 +1,7 @@
 <?php
 require('functions.php');
 $con= getConnection();
+
 ?>
 
 <!DOCTYPE html>
@@ -11,11 +12,14 @@ $con= getConnection();
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Ingresando Noticia</title>
 	<link rel="stylesheet" href="">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+	
+
 </head>
 <body>
 
 <form action="ingresarNoticias.php"  method="Post" name="formDatosPersonales">
-
+	<?php $user=['id_cedula'];?>
 	<label for="nombre">Nombre de la Noticia</label>
 	<input type="text" name="nombre"  id="nombre" placeholder="Escribe el nombre de la Noticia" required/>
 
